fd85dc9ec95bba719c77213f6779424aa52e0220
Александр <git@github.com:Aleksandr-64/main.py.git>
2024-09-01 20:32:27 +0400

    Hard task

---
+++ module_2_hard.py
@@ -0,0 +1,15 @@
+def password(numbers):
+    result = []
+    for i in range(1, numbers+1):
+        for j in range(i+1, numbers+1):
+            sum_ = i + j
+            if numbers % sum_ == 0:
+                result.append(i)
+                result.append(j)
+    return result
+
+
+numbers = int(input('Введите число из первой ячейки:'))
+result = password(numbers)
+
+print('Пароль:',*result, sep='')
