57eaa58d73ae861a0294b3132d22e97cbbeda842
Aleksandr Kol <sany199910@mail.ru>
2024-09-01 12:22:29 +0400

    Add files via upload

---
+++ modul_2_2.py
@@ -0,0 +1,21 @@
+#Первые вводные числа
+first = 123
+second = 456
+third = 789
+if first == second or first == third or second == third:
+    print(2)
+elif first == second == third:
+    print(3)
+else:
+    print(0)
+
+# Вторые вводные числа
+first = 42
+second = 69
+third = 42
+if first == second or first == third or second == third:
+    print(2)
+elif first == second == third:
+    print(3)
+else:
+    print(0)
